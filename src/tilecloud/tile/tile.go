dbe8710c40d0c5a8c2ceb5cf378e34cb20440357
Tom Payne <twpayne@gmail.com>
2012-11-25 12:51:35 +0100

    Refine tile.Tile interface

--- src/tilecloud/tile/tile.go
+++ src/tilecloud/tile/tile.go
@@ -1,13 +1,9 @@
 package tile
 
-type Tile struct {
-	Coord
-	contentEncoding string
-	contentType     string
-	data            string
-	err             error
-}
-
-func (p *Tile) Error() string {
-	return p.err.Error()
+type Tile interface {
+	Coord() Coord
+	ContentEncoding() string
+	ContentType() string
+	Data() []byte
+	Error() string
 }
