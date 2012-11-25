f071cd220ce6ec1f5c707fdc2cb49247f09662c9
Tom Payne <twpayne@gmail.com>
2012-11-25 11:02:08 +0100

    Add Rect

--- src/tilecloud/geom/rectangle.go
+++ src/tilecloud/geom/rectangle.go
@@ -4,6 +4,10 @@ type Rectangle struct {
 	Min, Max Point
 }
 
+func Rect(minX, minY, maxX, maxY float64) Rectangle {
+	return Rectangle{Point{minX, minY}, Point{maxX, maxY}}
+}
+
 func (r *Rectangle) Contains(p *Point) bool {
 	return r.Min.X <= p.X && p.X <= r.Max.X && r.Min.Y <= p.Y && p.Y <= r.Max.Y
 }
